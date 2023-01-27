ad32a055277181ce1d35015c7d50e99425f578cb
azharjk <azhr.project@gmail.com>
2023-01-27 18:02:29 +0700

    checkstyle check

--- src/main/java/com/github/azharjk/joane/users/User.java
+++ src/main/java/com/github/azharjk/joane/users/User.java
@@ -1,12 +1,16 @@
 package com.github.azharjk.joane.users;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 
 @Entity
-@Table(name="users")
+@Table(name = "users")
 public class User {
   @Id
-  @GeneratedValue(strategy=GenerationType.AUTO)
+  @GeneratedValue(strategy = GenerationType.AUTO)
   private Long id;
   private String email;
   private String password;
